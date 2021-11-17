0ee4bde97379d72986e6c1bef13b30172688047d
Henrik <henriktaog3@gmail.com>
2021-11-17 12:37:24 +0100

    initial world map

--- test.js
+++ test.js
@@ -1,10 +1,14 @@
-function changeColor() {
-    d3.select("#blue")
-      .attr("fill", "yellow");
-  }
-  
-  function changeRadius() {
-    d3.selectAll(".red")
-      .attr("r", 10);
-  }
-  
+const width = 900
+const height = 900
+
+const svg = d3.select('body').append('svg').attr('width', width).attr('height', height).attr('class', 'map')
+
+const projection = d3.geoMercator().scale(130).translate([width / 2 , height / 2])
+const path = d3.geoPath(projection)
+
+
+d3.json("https://unpkg.com/world-atlas@1/world/110m.json").then(data => {
+  const countries = topojson.feature(data, data.objects.countries)
+
+  svg.append("g").attr('class', 'countries').selectAll('path').data(countries.features).enter().append('path').attr('d', path)
+})
