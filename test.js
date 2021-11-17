0cea4c7a91f8a5d44e2b023bfbfb24b3998422b0
Lucy <lucy@360player.com>
2021-11-17 12:36:46 +0100

    added year filtering on dataset

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+var currentYear = 2021
+
 function changeColor() {
     d3.select("#blue")
       .attr("fill", "yellow");
@@ -11,6 +13,17 @@ function changeColor() {
   function logData() {
     var data = d3.json("owid-co2-data.json");
     console.log(data);
+  }
+
+  function filterYear(year) {
+    var dat = d3.csv("owid-co2-data.csv").then(function(csv) {
+      csv = csv.filter(function(row) {
+          return row['year'] == year
+      });
+      console.log(csv)
+      currentYear = year
+      return csv
+      });
   }
   
   
