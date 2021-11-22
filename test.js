b1cdd8cd1e42aa44f78c6960385d2ff1ee22f5cd
Henrik <henriktaog3@gmail.com>
2021-11-22 22:23:03 +0100

    now uses promises

--- test.js
+++ test.js
@@ -6,9 +6,15 @@ const svg = d3.select('body').append('svg').attr('width', width).attr('height', 
 const projection = d3.geoMercator().scale(100).translate([width / 2 , height / 1.5])
 const path = d3.geoPath(projection)
 
+const promises = [
+  d3.json("https://unpkg.com/world-atlas@1/world/110m.json")
+]
 
-d3.json("https://unpkg.com/world-atlas@1/world/110m.json").then(data => {
-  const countries = topojson.feature(data, data.objects.countries)
+Promise.all(promises).then(ready)
 
-  svg.append("g").attr('class', 'countries').selectAll('path').data(countries.features).enter().append('path').attr('d', path)
-})
+function ready([world]){
+  svg.append("g")
+  .attr("class","countries")
+  .selectAll("path")
+  .data(topojson.feature(world, world.objects.countries).features).enter().append("path").attr("d", path)
+}
