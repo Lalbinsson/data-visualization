a3c433d296bfcb778709bb154d18f5fa4b763e0f
Lucy <lucy@360player.com>
2021-11-24 14:18:50 +0100

    class FilterHandler

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+import { FilterHandler } from './filterHandler.js'; 
+
 var width = 650
 var height = 400
 
@@ -10,6 +12,51 @@ var path = d3.geoPath(projection)
 var promises = [
   d3.json("https://unpkg.com/world-atlas@1/world/110m.json")
 ]
+
+var emissionTypes = ["coal_co2", "gas_co2", "oil_co2", "cement_co2", "flaring_co2", "other_industry_co2"]
+var years = ["1950", "1960", "1970"]
+
+// initializing the FilterHandler-class with defaultValues
+const defaultCountries = []
+const defaultYears = ["2000", "1999"]
+var defaultEmissions = "gas_co2"
+var defaultFilteredData = []
+var filterHandler = new FilterHandler(defaultFilteredData, defaultEmissions, defaultCountries, defaultYears);
+
+Promise.all(promises).then(ready)
+
+// adding the options to the button
+d3.select("#emission-selector")
+  .selectAll()
+  .data(emissionTypes)
+  .enter()
+  .append('option')
+  .text(function (d) { return d; }) // text showed in the menu
+  .attr("value", function (d) { return d; }) 
+
+  d3.select("#emission-selector").on("change", function() {
+    var selectedEmissions = d3.select(this).property("value"); //kolla på att läsa in fler värden från checkboxen.
+    filterHandler.updateEmissions(selectedEmissions)
+    console.log(filterHandler.getEmissions())
+    filterHandler.filterDataSetOnCurrentFilters() //should be called on all global updates to update all filters()
+})
+
+
+// adding the options to the button
+d3.select("#year-selector")
+  .selectAll()
+  .data(years)
+  .enter()
+  .append('option')
+  .text(function (d) { return d; }) // text showed in the menu
+  .attr("value", function (d) { return d; }) 
+
+  d3.select("#year-selector").on("change", function() {
+    var selectedYear = d3.select(this).property("value"); //kolla på att läsa in fler värden från checkboxen.
+    filterHandler.updateYears(selectedYear)
+    console.log(filterHandler.getYears())
+    filterHandler.filterDataSetOnCurrentFilters() //should be called on all global updates to update all filters()
+})
 
 
 Promise.all(promises).then(ready)
