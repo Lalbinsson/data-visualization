22133c890da82c8d845d0262a3c619ecd36d7635
Henrik <henriktaog3@gmail.com>
2021-11-30 16:02:39 +0100

    added interaction between map and global settings. Some css

--- javascript/main.js
+++ javascript/main.js
@@ -1,118 +1,167 @@
-import { FilterHandler } from './filterHandler.js'; 
-import { lineChart } from './lineChart.js';
-import { drawpWorldMap } from './worldMap.js';
+import { FilterHandler } from './filterHandler.js'
+import { lineChart } from './lineChart.js'
+import { drawpWorldMap } from './worldMap.js'
 
 //TODO: read these from the data set
-var allEmissions = ["coal_co2", "gas_co2", "oil_co2", "cement_co2", "flaring_co2", "other_industry_co2"]
-var allYears = ["1950", "1960", "1970"]
-var allCountries = ["Sweden", "Finland", "Denmark", "China"]
+var allEmissions = [
+  'coal_co2',
+  'gas_co2',
+  'oil_co2',
+  'cement_co2',
+  'flaring_co2',
+  'other_industry_co2'
+]
+var allYears = ['1950', '1960', '1970']
+var allCountries = []
+
+const countryNameAccessor = d => d.properties['NAME']
+const countryIdAccessor = d => d.properties['ADM0_A3_IS']
+
+var promises = [
+  d3.json('../world-geojson.json'),
+  d3.csv('../owid-co2-data.csv')
+]
+
+var promises = Promise.all(promises)
 
 // initializing the FilterHandler-class with defaultValues
 var selectedCountries = []
-var selectedYear = ""
+var selectedYear = ''
 var selectedEmissions = []
 var defaultFilteredData = []
-var filterHandler = new FilterHandler(defaultFilteredData, selectedEmissions, selectedCountries, selectedYear);
+var filterHandler = new FilterHandler(
+  defaultFilteredData,
+  selectedEmissions,
+  selectedCountries,
+  selectedYear
+)
 
-d3.select("#year-selector")
+d3.select('#year-selector')
   .selectAll()
   .data(allYears)
   .enter()
   .append('li')
-  .text(function (d) { return d; }) // text showed in the menu
-  .attr("value", function (d) { return d; }) 
+  .text(function (d) {
+    return d
+  }) // text showed in the menu
+  .attr('value', function (d) {
+    return d
+  })
 
-  d3.select("#year-selector").on("change", function() {
-    var newYear = d3.select(this).property("value");
-    filterHandler.updateYear(newYear)
-    console.log(filterHandler.getYear())
+d3.select('#year-selector').on('change', function () {
+  var newYear = d3.select(this).property('value')
+  filterHandler.updateYear(newYear)
+  console.log(filterHandler.getYear())
 })
 
-d3.select("#countries-dropdown")
-  .selectAll()
-  .data(allCountries)
-  .enter()
-.append("li")
-.text(function (d) { return d; }) // text showed in the menu
-.append("input")
-.attr("type", "checkbox")
-.attr("id", function(d) { return d; })
-.style("float","left")   //move box left of label
-.on('change', function() {
-  console.log("clicked on ", this.id)
-  if(this.checked) {
-    if (!selectedCountries.includes(this.id)){
-      selectedCountries.push(this.id)
-    }
-  } else {
-    if (selectedCountries.includes(this.id)) {
-      const index = selectedCountries.indexOf(this.id);
-      if (index > -1) {
-        selectedCountries.splice(index, 1);
-      }
-    }
+promises.then(function ([worldMap]) {
+  for (let i = 0; i < worldMap.features.length; i++) {
+    if (isNaN(countryIdAccessor(worldMap.features[i])))
+      allCountries.push(countryIdAccessor(worldMap.features[i]))
   }
-  //update countries in filterHandler
-  //osäker på hur vi ska få detta att gå åt båda hållen så att boxes blir unchecked om man väljer det på kartan, tror att vi kanske bara kan selecta det elementet och sätta checked till false eller något.
-  filterHandler.updateCountries(selectedCountries)
-  console.log(filterHandler.getCountries);
+  allCountries.sort()
+
+  d3.select('#countries-dropdown')
+    .selectAll()
+    .data(allCountries)
+    .enter()
+    .append('button')
+    .attr('class', 'btn btn-success')
+    .attr('id', 'dropdown_elements')
+    .on('click', function (d) {
+      addSelectedCountry(d)
+    })
+    .text(function (d) {
+      return d
+    }) // text showed in the menu
+    .append('input')
+    .attr('type', 'checkbox')
+    .attr('vertical-align', 'middle')
+    .attr('id', function (d) {
+      return d
+    })
+    .style('float', 'left') //move box left of label
+    .on('change', function () {
+      console.log('clicked on ', this.id)
+      if (this.checked) {
+        if (!selectedCountries.includes(this.id)) {
+          selectedCountries.push(this.id)
+        }
+      } else {
+        if (selectedCountries.includes(this.id)) {
+          const index = selectedCountries.indexOf(this.id)
+          if (index > -1) {
+            selectedCountries.splice(index, 1)
+          }
+        }
+      }
+      //update countries in filterHandler
+      //osäker på hur vi ska få detta att gå åt båda hållen så att boxes blir unchecked om man väljer det på kartan, tror att vi kanske bara kan selecta det elementet och sätta checked till false eller något.
+      filterHandler.updateCountries(selectedCountries)
+      console.log(filterHandler.getCountries)
+    })
 })
 
-d3.select("#emissions-dropdown")
+d3.select('#emissions-dropdown')
   .selectAll()
   .data(allEmissions)
   .enter()
-.append("li")
-.text(function (d) { return d; }) // text showed in the menu
-.append("input")
-.attr("type", "checkbox")
-.attr("class", "checkedEmissions")
-.attr("id", function(d) { return d; })
-.style("float","left")   //move box left of label
-.on('change', function() {
-  if(this.checked) {
-    if (!selectedEmissions.includes(this.id)){
+  .append('button')
+  .attr('class', 'btn btn-success')
+  .attr('id', 'dropdown_elements')
+  .text(function (d) {
+    return d
+  }) // text showed in the menu
+  .append('input')
+  .attr('type', 'checkbox')
+  .attr('class', 'checkedEmissions')
+  .attr('id', function (d) {
+    return d
+  })
+  .style('float', 'left') //move box left of label
+  .on('change', updateDropdown)
+
+function updateDropdown () {
+  if (this.checked) {
+    if (!selectedEmissions.includes(this.id)) {
       selectedEmissions.push(this.id)
     }
   } else {
     if (selectedEmissions.includes(this.id)) {
-      const index = selectedEmissions.indexOf(this.id);
+      const index = selectedEmissions.indexOf(this.id)
       if (index > -1) {
-        selectedEmissions.splice(index, 1);
+        selectedEmissions.splice(index, 1)
       }
     }
   }
 
   filterHandler.updateEmissions(selectedEmissions)
-  console.log(filterHandler.getEmissions());
-})
-
+  console.log(filterHandler.getEmissions())
+}
 
 //gör dropdown-listorna hidden/visible på click, fixa så att de inte tar upp hela ytan när de är hidden.
 var checkListCountries = document.getElementById('countries-selector')
 var countryList = document.getElementsByClassName('countries')[0]
 checkListCountries.onclick = function (evt) {
-    if (countryList.style.visibility == "visible")
-        countryList.style.visibility = "hidden";
-    else
-        countryList.style.visibility = "visible";
+  if (countryList.style.visibility == 'visible')
+    countryList.style.visibility = 'hidden'
+  else countryList.style.visibility = 'visible'
 }
 
-checkListCountries.onblur = function(evt) {
-    countryList.style.visibility = "hidden";
+checkListCountries.onblur = function (evt) {
+  countryList.style.visibility = 'hidden'
 }
 
 var checkListEmissions = document.getElementById('emissions-selector')
 var emissionList = document.getElementsByClassName('emissions')[0]
 checkListEmissions.onclick = function (evt) {
-    if (emissionList.style.visibility == "visible")
-        emissionList.style.visibility = "hidden";
-    else
-        emissionList.style.visibility = "visible";
+  if (emissionList.style.visibility == 'visible')
+    emissionList.style.visibility = 'hidden'
+  else emissionList.style.visibility = 'visible'
 }
 
-checkListEmissions.onblur = function(evt) {
-    emissionList.style.visibility = "hidden";
+checkListEmissions.onblur = function (evt) {
+  emissionList.style.visibility = 'hidden'
 }
 
 /*
@@ -130,5 +179,18 @@ var currentYear = 2021
   }
   */
 
-drawpWorldMap()
+function addSelectedCountry (country) {
+  var index = selectedCountries.indexOf(country)
+  var element = document.getElementById(country)
+  if (index !== -1) {
+    selectedCountries.splice(index, 1)
+    element.checked = false
+  } else {
+    selectedCountries.push(country)
+    element.checked = true
+  }
+  filterHandler.updateCountries(selectedCountries)
+}
+
+drawpWorldMap(addSelectedCountry, promises)
 lineChart()
--- javascript/worldMap.js
+++ javascript/worldMap.js
@@ -1,50 +1,44 @@
-export async function drawpWorldMap (){
-  var promises = [
-    d3.json("../world-geojson.json"),
-    d3.csv("../owid-co2-data.csv")
-  ]
+export async function drawpWorldMap (addSelectedCountry, promises) {
+  var selectedCountries
 
   const countryNameAccessor = d => d.properties['NAME']
-  const countryIdAccessor = d => d.properties["ADM0_A3_IS"]
-  const metric = "share_global_co2"
-  var year = "2020"
+  const countryIdAccessor = d => d.properties['ADM0_A3_IS']
+  const metric = 'share_global_co2'
+  var year = '2020'
 
   const width = 650
-  const height= 400
+  const height = 500
 
-  const projection2 = d3.geoMercator().scale(100).translate([width/2.1, height/1.5])
+  const projection2 = d3
+    .geoMercator()
+    .scale(100)
+    .translate([width / 2.1, height / 1.7])
 
   const pathGenerator = d3.geoPath(projection2)
 
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .style('background', '#afdbdb')
+    .attr('width', width)
+    .attr('height', height)
 
-  const wrapper = d3.select("#wrapper")
-    .append("svg")
-    .style("background", "#afdbdb")
-    .attr("width", width)
-    .attr("height", height)
+  const bounds = wrapper
+    .append('g')
+    .style('transform', `translate(${10}px, ${10}px)`)
 
+  promises.then(ready)
 
-  const bounds = wrapper.append("g")
-    .style("transform", `translate(${
-      10
-    }px, ${
-      10
-    }px)`)
+  const tooltip = d3.select('#tooltip')
 
-  Promise.all(promises).then(ready)
-
-  const tooltip = d3.select("#tooltip")
-
-  function filter(co2_dataset, metricDataByCountry) {
+  function filter (co2_dataset, metricDataByCountry) {
     co2_dataset.forEach(d => {
-      if (d["iso_code"] == "OWID_WRL" || d["year"] != year) return
-      metricDataByCountry[d["iso_code"]] = + d ["share_global_co2"]
+      if (d['iso_code'] == 'OWID_WRL' || d['year'] != year) return
+      metricDataByCountry[d['iso_code']] = +d['share_global_co2']
     })
   }
 
-  function ready([worldMap, co2_dataset]){
-
-    console.log(worldMap)
+  function ready ([worldMap, co2_dataset]) {
     let metricDataByCountry = {}
 
     filter(co2_dataset, metricDataByCountry)
@@ -52,104 +46,165 @@ export async function drawpWorldMap (){
     const metricValues = Object.values(metricDataByCountry)
     const metricValueExtent = d3.extent(metricValues)
 
-    const colorScale = d3.scaleLog()
-    .domain([0, metricValueExtent[0] + 0.01, metricValueExtent[1]])
-    .range(["white", "white", "darkgreen"])
+    const colorScale = d3
+      .scaleLog()
+      .domain([0, metricValueExtent[0] + 0.01, metricValueExtent[1]])
+      .range(['white', 'white', 'darkgreen'])
 
-    const countries = bounds.selectAll(".country").data(worldMap.features.filter(function(d){
-      return d.properties.NAME != "Antarctica"
-    })).enter().append("path")
-    .attr("class", "country")
-    .attr("d", pathGenerator)
-    .style("opacity", 1)
-    .style("stroke", "white")
-    .style("stroke-width", 0.7)
-    .attr("fill", d => {
-    const metricValue = metricDataByCountry[countryIdAccessor(d)]
-    if(typeof metricValue == "undefined") return "#e2e6e9"
-    return colorScale(metricValue)
-    })
-    
-    .on('mouseover',function(d){
-      d3.selectAll(".country")
-      .transition()
-      .duration(200)
-      .style("opacity", 0.35)
-      d3.select(this)
-      .transition()
-      .duration(200)
-        .style("opacity", 1.0)
-    })
-    .on('mouseout', function(d){
-      d3.selectAll(".country")
-      .transition()
-      .duration(200)
-      .style("opacity", 1)
-      d3.select(this)
-        .style("opacity", 0.9)
-        .style("stroke","white")
-        .style("stroke-width", 0.3);
-    })
-    .on("mouseenter", onMouseEnter)
-    .on("mouseleave", onMouseLeave)
+    const countries = bounds
+      .selectAll('.country')
+      .data(
+        worldMap.features.filter(function (d) {
+          return d.properties.NAME != 'Antarctica'
+        })
+      )
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', pathGenerator)
+      .style('opacity', 1)
+      .style('stroke', 'white')
+      .style('stroke-width', 0.7)
+      .attr('fill', d => {
+        const metricValue = metricDataByCountry[countryIdAccessor(d)]
+        if (typeof metricValue == 'undefined') return '#e2e6e9'
+        return colorScale(metricValue)
+      })
 
+      .on('mouseover', function (d) {
+        d3.selectAll('.country')
+          .transition()
+          .duration(200)
+          .style('opacity', 0.35)
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .style('opacity', 1.0)
+      })
+      .on('mouseout', function (d) {
+        d3.selectAll('.country')
+          .transition()
+          .duration(200)
+          .style('opacity', 1)
+        d3.select(this)
+          .style('opacity', 0.9)
+          .style('stroke', 'white')
+          .style('stroke-width', 0.3)
+      })
+      .on(
+        'click',
+        (selectedCountries = function (d) {
+          var country = countryIdAccessor(d)
+          console.log(country)
+          addSelectedCountry(country)
+        })
+      )
+      .on('mouseenter', onMouseEnter)
+      .on('mouseleave', onMouseLeave)
 
-  function onMouseEnter (datum) {
-    tooltip.style("opacity", 1)
- 
-    const metricValue = metricDataByCountry[countryIdAccessor(datum)]
-    tooltip.select("#country").text(countryNameAccessor(datum))
-    tooltip.select("#value").text(`${d3.format(",.2f")(metricValue || 0)}%`)
+    function onMouseEnter (datum) {
+      tooltip.style('opacity', 1)
 
-    const [centerX, centerY] = pathGenerator.centroid(datum)
-    
-    const x = centerX + 10
-    const y = centerY + 10
+      const metricValue = metricDataByCountry[countryIdAccessor(datum)]
+      tooltip.select('#country').text(countryNameAccessor(datum))
+      tooltip.select('#value').text(`${d3.format(',.2f')(metricValue || 0)}%`)
 
-    tooltip.style("transform", `translate(`
-      + `calc( -50% + ${x}px),`
-      + `calc( 500% + ${y}px)`
-      + `)`)
-  }
+      const [centerX, centerY] = pathGenerator.centroid(datum)
 
-  function onMouseLeave() {
-    tooltip.style("opacity", 0)
-  }
+      const x = centerX + 10
+      const y = centerY + 10
 
-  var title = d3.select("#wm-title").append("text").attr("id", "yearTitle").text(year)
+      tooltip.style(
+        'transform',
+        `translate(` + `calc( -50% + ${x}px),` + `calc( 500% + ${y}px)` + `)`
+      )
+    }
 
-  var slider = d3
+    function onMouseLeave () {
+      tooltip.style('opacity', 0)
+    }
+
+    var title = d3
+      .select('#wm-title')
+      .append('text')
+      .attr('id', 'yearTitle')
+      .text(year)
+
+    var slider = d3
       .sliderHorizontal()
       .tickFormat(d3.format('.0f'))
       .displayValue(year)
-      .fill(["#EE3234"])
-      .default(["2021"])
+      .fill(['green'])
+      .default(['2021'])
       .ticks(12)
-      .tickValues([1900, 1912, 1924, 1936, 1948, 1960, 1972, 1984, 1996, 2008, 2020])
+      .tickValues([
+        1900,
+        1912,
+        1924,
+        1936,
+        1948,
+        1960,
+        1972,
+        1984,
+        1996,
+        2008,
+        2020
+      ])
       .min(1902)
       .max(2021)
       .step(1)
       .width(530)
       .displayValue(false)
-      .on('onchange', (val) => {
-        year=val
-        d3.select("#yearTitle").text(year)
+      .on('onchange', val => {
+        year = val
+        d3.select('#yearTitle').text(year)
         filter(co2_dataset, metricDataByCountry)
-        bounds.selectAll(".country")
-        .attr("fill", d => {
+        bounds.selectAll('.country').attr('fill', d => {
           const metricValue = metricDataByCountry[countryIdAccessor(d)]
-          if(typeof metricValue == "undefined") return "#e2e6e9"
+          if (typeof metricValue == 'undefined') return '#e2e6e9'
           return colorScale(metricValue)
-          })
+        })
         console.log(year)
       })
-  
+
     d3.select('#slider')
       .append('svg')
       .attr('width', 600)
       .attr('height', 80)
       .append('g')
       .attr('transform', 'translate(30,30)')
-      .call(slider);
+      .call(slider)
+
+    var svg = d3.select('svg')
+
+    svg
+      .append('g')
+      .attr('class', 'legendLog')
+      .attr('transform', 'translate(20,440)')
+      .on('mouseover', function (d) {})
+
+    var legendLog = d3
+      .legendColor()
+      .shapeWidth(40)
+      .shapePadding(10)
+      .title('Quantiles')
+      .cells(10)
+      .labels([
+        '0-10',
+        '10-20',
+        '20-30',
+        '30-40',
+        '40-50',
+        '50-60',
+        '60-70',
+        '70-80',
+        '80-90',
+        '90-100'
+      ])
+      .orient('horizontal')
+      .scale(colorScale)
+
+    svg.select('.legendLog').call(legendLog)
   }
+  return selectedCountries
 }
